aecaa75cd93a124753374d87ce35526e196ad35e
jphiri <jphiri@myune.edu.au>
2024-07-30 13:37:32 +1000

    Created the master layout, developed the index for listing blogs, and adjusted the Post controller to display the index view.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    This is content for index file
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My First Blog</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>>
+</html> 
